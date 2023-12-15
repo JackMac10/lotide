d7900b534c4c42569b2bbc6767726ba9425a7333
JackMac10 <macdonaldjackson0@gmail.com>
2023-12-15 15:26:03 -0800

    assertEqual created

---
+++ assertEqual.js
@@ -0,0 +1,10 @@
+const assertEqual = function(actual, expected) {
+  if (actual === expected) {
+    console.log(`🟢🟢🟢 Assertion Passed: ${actual} === ${expected}`);
+  }
+  else if (actual !== expected) {
+    console.log(`🚫🚫🚫 Assertion Failed: ${actual} !== ${expected}`);
+  }
+
+};
+module.exports = assertEqual;
---
+++ const sum = function(a, b) {.js
@@ -0,0 +1,6 @@
+const sum = function(a, b) { 
+  return a + b;
+}
+
+// TEST CODE
+console.assert(sum(1, 2) === 3);

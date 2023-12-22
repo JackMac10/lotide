890eaa29363e0ff84bf819d9ffecdbc4abd9f1c3
JackMac10 <macdonaldjackson0@gmail.com>
2023-12-22 13:47:22 -0800

    head.js created, functional, eslinted

---
+++ tail.js
@@ -0,0 +1,29 @@
+const assertEqual = function(actual, expected) {
+  if (actual === expected) {
+    console.log(`🟢🟢🟢 Assertion Passed: ${actual} === ${expected}`);
+  }
+  else if (actual !== expected) {
+    console.log(`🚫🚫🚫 Assertion Failed: ${actual} !== ${expected}`);
+  }
+
+};
+
+const tail = function(array) {
+  const result = [];
+  for (let i = 1; i < array.length; i++) { // for(const item of array ){
+    const item = array[i];
+    result.push(item);
+  }
+  return result;
+};
+
+
+
+const result = tail(["Hello", "Lighthouse", "Labs"]);
+assertEqual(result.length, 2); // ensure we get back two elements
+assertEqual(result[0], "Lighthouse"); // ensure first element is "Lighthouse"
+assertEqual(result[1], "Labs"); //
+
+const words = ["Yo Yo", "Lighthouse", "Labs"];
+tail(words); // no need to capture the return value since we are not checking it
+assertEqual(words.length, 3); //

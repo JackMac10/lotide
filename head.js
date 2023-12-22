58b7de319519f102186326d12b6eeb91da01047b
JackMac10 <macdonaldjackson0@gmail.com>
2023-12-22 11:49:36 -0800

    head.js created and functional

---
+++ head.js
@@ -0,0 +1,20 @@
+const assertEqual = function(actual, expected) {
+  if (actual === expected) {
+    console.log(`🟢🟢🟢 Assertion Passed: ${actual} === ${expected}`);
+  }
+  else if (actual !== expected) {
+    console.log(`🚫🚫🚫 Assertion Failed: ${actual} !== ${expected}`);
+  }
+
+};
+module.exports = assertEqual;
+
+
+function head(array) {
+return array.[0]
+}
+
+
+assertEqual(head([5,6,7]), 5);
+assertEqual(head(["Hello", "Lighthouse", "Labs"]), "Hello");
+

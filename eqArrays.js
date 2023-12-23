279dc01f64b416f0f7061aaa0aaa04eca9693af2
JackMac10 <macdonaldjackson0@gmail.com>
2023-12-22 16:35:43 -0800

    eqArrays created, functional, eslinted

---
+++ eqArrays.js
@@ -0,0 +1,35 @@
+const assertEqual = function (actual, expected) {
+  if (actual === expected) {
+    console.log(`🟢🟢🟢 Assertion Passed: ${actual} === ${expected}`);
+  }
+  else if (actual !== expected) {
+    console.log(`🚫🚫🚫 Assertion Failed: ${actual} !== ${expected}`);
+  }
+
+};
+
+
+const eqArrays = function(array, array2) {
+  let result = null;
+  if (array.length !== array2.length); {
+    result = false;
+  }
+  for (let i = 0; i < array.length; i++) {
+    if (array[i] !== array2[i]) {
+      return false;
+    }
+    else {
+      result = true;
+    }
+  }
+  return result;
+};
+
+
+console.log(eqArrays([1, 2, 3], [1, 2, 3])); // => true
+console.log(eqArrays([1, 2, 3], [3, 2, 1])); // => false
+
+console.log(eqArrays(["1", "2", "3"], ["1", "2", "3"])); // => true
+console.log(eqArrays(["1", "2", "3"], ["1", "2", 3])); // => false
+
+assertEqual(eqArrays([1, 2, 3], [1, 2, 3]), true);

9241c52e4c9074c7c3eca1a1d9944fba02b4eb1d
JackMac10 <macdonaldjackson0@gmail.com>
2024-01-19 16:47:11 -0800

    eqArrays tested and refactored

--- eqArrays.js
+++ eqArrays.js
@@ -1,12 +1,5 @@
-// const assertEqual = function (actual, expected) {
-//   if (actual === expected) {
-//     console.log(`🟢🟢🟢 Assertion Passed: ${actual} === ${expected}`);
-//   }
-//   else if (actual !== expected) {
-//     console.log(`🚫🚫🚫 Assertion Failed: ${actual} !== ${expected}`);
-//   }
+const assertEqual = require(`./assertEqual`)
 
-// };
 
 
 const eqArrays = function (array, array2) {
@@ -29,9 +22,5 @@ const eqArrays = function (array, array2) {
   }
 };
 
+module.exports = eqArrays
 
-console.log(eqArrays(["1", 2, 3], [1, 2, 3])); // => true
-console.log(eqArrays([1, 2, 3], [3, 2, 1])); // => false
-
-console.log(eqArrays(["1", "2", "3"], ["1", "2", "3"])); // => true
-console.log(eqArrays(["1", "2", "3"], ["1", "2", 3])); // => false
